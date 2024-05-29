4f892bbb256f71dc4353a8364da924a1170c7a59
Larissa <larissagomes8020@gmail.com>
2024-05-29 16:22:26 -0300

    Extrai ano e mês emque a obra foi realizada e calcula a média das classificações para cada mês em cada ano

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import pandas as pd
 
 df = pd.read_excel('dados_parte_interessada.xlsx')
+
+df['ano'] = df['data de início'].dt.year
+df['mes'] = df['data de início'].dt.month
+
+media_classificação = df.groupby(['ano', 'mes'])['classificação'].mean
 print(df)
