0aaeddd677a1e2440da10c7c2a751da50798cc17
Larissa <larissagomes8020@gmail.com>
2024-05-29 17:30:17 -0300

    encontrar meses com maior e menor média de satisfação

--- main.py
+++ main.py
@@ -5,5 +5,11 @@ df = pd.read_excel('dados_parte_interessada.xlsx')
 df['ano'] = df['data de início'].dt.year
 df['mes'] = df['data de início'].dt.month
 
-media_classificação = df.groupby(['ano', 'mes'])['classificação'].mean
-print(df)
+#reset_index - para garantir o padrão dos index
+media_classificação = df.groupby(['ano', 'mes'])['classificação'].mean().reset_index()
+
+meses_maior_satisfação = media_classificação.loc[media_classificação.groupby('ano')['classificação'].idxmax()]
+print(meses_maior_satisfação)
+
+meses_menor_satisfação = media_classificação.loc[media_classificação.groupby('ano')['classificação'].idxmin()]
+print(meses_menor_satisfação)
