dfbc29ac888177f848d0cd9d54c560b9afb0bc5b
Larissa <larissagomes8020@gmail.com>
2024-05-30 14:09:37 -0300

    Mostrar quantas vezes cada cliente contrartou a empresa

--- main.py
+++ main.py
@@ -10,9 +10,7 @@ df['mes'] = df['data de início'].dt.month
 media_classificação = df.groupby(['ano', 'mes'])['classificação'].mean().reset_index()
 
 meses_maior_satisfação = media_classificação.loc[media_classificação.groupby('ano')['classificação'].idxmax()]
-
 meses_menor_satisfação = media_classificação.loc[media_classificação.groupby('ano')['classificação'].idxmin()]
-
 
 df['lucro'] = df.apply(lambda x : x['valor final da obra'] * 0.03, axis=1)
 df['preço cobrado'] = df.apply(lambda x : x['valor final da obra'] + x['lucro'], axis=1)
@@ -20,3 +18,4 @@ df['preço cobrado'] = df.apply(lambda x : x['valor final da obra'] + x['lucro']
 df_meses_menor_lucro = df.loc[df.groupby('ano')['lucro'].idxmin()] 
 df_meses_maior_lucro = df.loc[df.groupby('ano')['lucro'].idxmax()]
 
+df_clientela = df['clientes'].value_counts().reset_index()
